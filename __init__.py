9167421ea0314b2629dece76daf1c5d62827d152
wrongu <richard.d.lange.13@dartmouth.edu>
2022-08-30 08:12:20 -0400

    __init__ trickery to make package-relative imports work outside of open_lth

--- __init__.py
+++ __init__.py
@@ -2,3 +2,7 @@
 
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
+
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
